c2bf1d7f1a15c7b5bbd6f32943c7d69a12d67d55
Tommy Bidne <tbidne@protonmail.com>
2023-02-17 23:31:34 +1300

    Add multiple ghc versions for safe-exceptions

--- lib.nix
+++ lib.nix
@@ -2,10 +2,15 @@
 
 let
   ghc-to-nixpkgs = {
+    ghc8107 = "aa1d74709f5dac623adb4d48fdfb27cc2c92a4d4";
     ghc902 = "b7d8c687782c8f9a1d425a7e486eb989654f6468";
     ghc925 = "545c7a31e5dedea4a6d372712a18e00ce097d462";
     ghc944 = "545c7a31e5dedea4a6d372712a18e00ce097d462";
   };
+
+  showKeys = attrs:
+    let attrKeys = builtins.attrNames attrs;
+    in builtins.concatStringsSep ", " attrKeys;
 
   hls = dev: compiler: pkgs:
     if dev then [
@@ -28,7 +33,13 @@ in
   # Retrieves nixpkgs corresponding to the given ghc version.
   getPkgs = ghcVers:
     let
-      hash = ghc-to-nixpkgs.${ghcVers};
+      hash =
+        if ghc-to-nixpkgs ? ${ghcVers}
+        then ghc-to-nixpkgs.${ghcVers}
+        else
+          throw
+            ''Invalid ghcVers: '${ghcVers}'; valid keys are: ${showKeys ghc-to-nixpkgs}.
+              '';
     in
     import
       (fetchTarball {
