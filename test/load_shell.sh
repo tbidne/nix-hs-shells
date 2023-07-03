a300d6a0ccd76c83821f1a7b68750fa6e69e40df
Tommy Bidne <tbidne@protonmail.com>
2023-07-03 15:04:40 +1200

    Load non-hls tools on CI

--- test/load_shell.sh
+++ test/load_shell.sh
@@ -3,6 +3,17 @@ set +e
 # Test every ghc version w/ all optional tools. Obviously this can take a very
 # long time.
 
+parse_bool (){
+  if [[ $2 == "true" ]]; then
+    echo "--arg $1 true"
+  elif [[ $2 == "false" ]]; then
+    echo "--arg $1 false"
+  else
+    echo "Unexpected arg: '$2'. Expected one of (true | false)."
+    exit 1
+  fi
+}
+
 export LANG="C.UTF-8"
 
 export ghcVersions="
@@ -27,22 +38,46 @@ ormolu="--arg ormolu true"
 verbose=0
 while [ $# -gt 0 ]; do
     if [[ $1 == "--help" || $1 == "-h" ]]; then
-      echo -e "load_shell: Loads shells with all options enabled.\n"
-      echo "Usage: load_shell.sh [-d|--dry-run]"
+      echo -e "load_shell: Loads haskell shell.\n"
+      echo "Usage: load_shell.sh [--apply-refact (true | false)]"
+      echo "                     [-d|--dry-run]"
+      echo "                     [--fourmolu (true | false)]"
       echo "                     [--ghc GHC]"
+      echo "                     [--hlint (true | false)]"
+      echo "                     [--hls (true | false)]"
       echo "                     [--no-tools]"
+      echo "                     [--ormolu (true | false)]"
       echo "                     [-h|--help]"
       echo "                     [-v|--verbose]"
       echo ""
       echo "Available options:"
-      echo -e "  --d,--dry-run \tRuns with --dry-run.\n"
-      echo -e "  --ghc         \tRuns with a specific ghc e.g. ghc902.\n"
-      echo -e "  --no-tools    \tDisables all tools.\n"
+      echo -e "  --apply-refact (true | false)  Enables apply-refact tool.\n"
+      echo -e "  --d,--dry-run                  Runs with --dry-run i.e. does not build"
+      echo -e "                                 anything.\n"
+      echo -e "  --fourmolu (true | false)      Enables fourmolu tool.\n"
+      echo -e "  --ghc                          Runs with a specific ghc e.g. ghc902. Otherwise"
+      echo -e "                                 tries all versions.\n"
+      echo -e "  --hlint (true | false)         Enables hlint tool.\n"
+      echo -e "  --hls (true | false)           Enables hls tool.\n"
+      echo -e "  --no-tools                     Disables all tools.\n"
+      echo -e "  --ormolu (true | false)        Enables ormolu tool.\n"
       exit 0
+    elif [[ $1 == "--apply-refact" ]]; then
+      apply_refact=$(parse_bool "applyRefact" $2)
+      shift
     elif [[ $1 == "--dry-run" || $1 == "-d" ]]; then
       cmd="--dry-run"
+    elif [[ $1 == "--fourmolu" ]]; then
+      fourmolu=$(parse_bool "fourmolu" $2)
+      shift
     elif [[ $1 == "--ghc" ]]; then
       ghcVersions=$2
+      shift
+    elif [[ $1 == "--hlint" ]]; then
+      hlint=$(parse_bool "hlint" $2)
+      shift
+    elif [[ $1 == "--hls" ]]; then
+      hls=$(parse_bool "hls" $2)
       shift
     elif [[ $1 == "--no-tools" ]]; then
       apply_refact="--arg applyRefact false"
@@ -50,6 +85,9 @@ while [ $# -gt 0 ]; do
       hlint="--arg hlint false"
       hls="--arg hls false"
       ormolu="--arg ormolu false"
+    elif [[ $1 == "--ormolu" ]]; then
+      ormolu=$(parse_bool "ormolu" $2)
+      shift
     elif [[ $1 == "--verbose" || $1 == "-v" ]]; then
       verbose=1
     else
