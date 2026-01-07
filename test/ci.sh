7c09e444719c1d862aa3bb3974c5994d106b54c0
Tommy Bidne <tbidne@protonmail.com>
2026-01-08 10:24:24 +1300

    Add ghc9123 and ghc9141

--- test/ci.sh
+++ test/ci.sh
@@ -20,18 +20,6 @@ tool_map[default,hlint]="true"
 tool_map[default,hls]="true"
 tool_map[default,ormolu]="true"
 
-tool_map[ghc981,apply-refact]="false" # unsupported
-tool_map[ghc981,fourmolu]="false" # poor caching
-tool_map[ghc981,hlint]="false" # poor caching
-tool_map[ghc981,hls]="false" # poor caching
-tool_map[ghc981,ormolu]="false" # poor caching
-
-tool_map[ghc983,apply-refact]="false" # poor caching
-tool_map[ghc983,fourmolu]="false" # poor caching
-tool_map[ghc983,hlint]="false" # poor caching
-tool_map[ghc983,hls]="false" # poor caching
-tool_map[ghc983,ormolu]="false" # poor caching
-
 tool_map[ghc9101,apply-refact]="false" # unsupported
 tool_map[ghc9101,hlint]="false" # unsupported
 
@@ -49,6 +37,18 @@ tool_map[ghc9121,fourmolu]="false" # unsupported
 tool_map[ghc9121,hlint]="false" # unsupported
 tool_map[ghc9121,hls]="false" # unsupported
 tool_map[ghc9121,ormolu]="false" # unsupported
+
+tool_map[ghc9123,apply-refact]="false" # unsupported
+tool_map[ghc9123,fourmolu]="false" # unsupported
+tool_map[ghc9123,hlint]="false" # unsupported
+tool_map[ghc9123,hls]="false" # unsupported
+tool_map[ghc9123,ormolu]="false" # unsupported
+
+tool_map[ghc9141,apply-refact]="false" # unsupported
+tool_map[ghc9141,fourmolu]="false" # unsupported
+tool_map[ghc9141,hlint]="false" # unsupported
+tool_map[ghc9141,hls]="false" # unsupported
+tool_map[ghc9141,ormolu]="false" # unsupported
 
 ghc_version=""
 tool_list="bare hls ormolu fourmolu hlint apply-refact"
